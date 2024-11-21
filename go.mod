c1eab1ef986f5304a7ac7e6ee61f666110f9bd87
Amadi Azdaev <amady833@gmail.com>
2024-11-21 07:55:05 +0300

    migrations

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.0
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 	github.com/lib/pq v1.10.9
+	github.com/pressly/goose/v3 v3.22.1
 	github.com/redis/go-redis/v9 v9.7.0
 	github.com/sirupsen/logrus v1.9.3
 )
@@ -12,6 +13,9 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/sethvargo/go-retry v0.3.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/sync v0.9.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 )
--- migrations/run_migrations.go
+++ migrations/run_migrations.go
@@ -1,41 +1,41 @@
-// package main
+package main
 
-// import (
-// 	"database/sql"
-// 	"embed"
-// 	"fmt"
-// 	"log"
-// 	"os"
+import (
+	"database/sql"
+	"embed"
+	"fmt"
+	"log"
+	"os"
 
-// 	_ "github.com/lib/pq"
-// 	"github.com/pressly/goose/v3"
-// )
+	_ "github.com/lib/pq"
+	"github.com/pressly/goose/v3"
+)
 
-// //go:embed *.sql
-// var embedMigrations embed.FS
+//go:embed *.sql
+var embedMigrations embed.FS
 
-// func main() {
-// 	psqlInfo := fmt.Sprintf(
-// 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-// 		os.Getenv("PG_HOST"),
-// 		os.Getenv("PG_PORT"),
-// 		os.Getenv("PG_USER"),
-// 		os.Getenv("PG_PASSWORD"),
-// 		os.Getenv("PG_DB_NAME"),
-// 	)
+func main() {
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("PG_HOST"),
+		os.Getenv("PG_PORT"),
+		os.Getenv("PG_USER"),
+		os.Getenv("PG_PASSWORD"),
+		os.Getenv("PG_DB_NAME"),
+	)
 
-// 	db, err := sql.Open("postgres", psqlInfo)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// 	goose.SetBaseFS(embedMigrations)
+	goose.SetBaseFS(embedMigrations)
 
-// 	if err := goose.SetDialect("postgres"); err != nil {
-// 		log.Fatal(err)
-// 	}
+	if err := goose.SetDialect("postgres"); err != nil {
+		log.Fatal(err)
+	}
 
-// 	if err := goose.Up(db, "."); err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
+	if err := goose.Up(db, "."); err != nil {
+		log.Fatal(err)
+	}
+}
